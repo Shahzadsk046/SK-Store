f92af104c036db9de06949a593cd42417e0a8d82
Shehzad Khan <55661874+Shahzadsk046@users.noreply.github.com>
2022-08-06 16:20:50 +0500

    Slider Component Completed

---
+++ src/data.js
@@ -0,0 +1,45 @@
+import Slide1 from "./images/slide1.jpg";
+import Slide2 from "./images/slide2.jpg";
+import Slide3 from "./images/slide3.jpg";
+
+export const slideritems = [
+  {
+    id: 1,
+    img: `${Slide1}`,
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: `${Slide2}`,
+    title: "AUTUMN SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: `${Slide3}`,
+    title: "POPULAR SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: `${Slide1}`,
+        title: "SHIRT STYLE!",
+    },
+    {
+        id: 2,
+        img: `${Slide2}`,
+        title: "LOUNGEWEAR LOVE",
+    },
+    {
+        id: 3,
+        img: `${Slide3}`,
+        title: "LIGHT JACKETS",
+    },
+]
