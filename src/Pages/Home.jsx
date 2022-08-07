8f9859552cb528e390706b5e7bb188e02da27fe2
Shehzad Khan <55661874+Shahzadsk046@users.noreply.github.com>
2022-08-07 11:29:32 +0500

    Newsletter Component Completed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Announcement from "../components/Announcement";
 import Categories from "../components/Categories";
 import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
 import Products from "../components/Products";
 import Slider from "../components/Slider";
 
@@ -11,8 +12,9 @@ const Home = () => {
       <Announcement />
       <Navbar />
       <Slider />
-      <Categories/>
-      <Products/>
+      <Categories />
+      <Products />
+      <Newsletter />
     </div>
   );
 };
