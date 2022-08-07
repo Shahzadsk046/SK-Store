3cc1ab91f9e00483b9e9a403c48ed39b807aa494
Shehzad Khan <55661874+Shahzadsk046@users.noreply.github.com>
2022-08-07 09:31:06 +0500

    Categories Frontend Completed

--- src/data.js
+++ src/data.js
@@ -1,6 +1,9 @@
 import Slide1 from "./images/slide1.jpg";
 import Slide2 from "./images/slide2.jpg";
 import Slide3 from "./images/slide3.jpg";
+import IMG1 from "./images/shirt1.webp";
+import IMG2 from "./images/loungewear1.jpg";
+import IMG3 from "./images/light_jacket1.webp";
 
 export const slideritems = [
   {
@@ -29,17 +32,17 @@ export const slideritems = [
 export const categories = [
     {
         id: 1,
-        img: `${Slide1}`,
+        img: `${IMG1}`,
         title: "SHIRT STYLE!",
     },
     {
         id: 2,
-        img: `${Slide2}`,
+        img: `${IMG2}`,
         title: "LOUNGEWEAR LOVE",
     },
     {
         id: 3,
-        img: `${Slide3}`,
+        img: `${IMG3}`,
         title: "LIGHT JACKETS",
     },
 ]
