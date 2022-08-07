51085d7a0bd28b2be805451c282da0149d67b32e
Shehzad Khan <55661874+Shahzadsk046@users.noreply.github.com>
2022-08-07 21:51:07 +0500

    Register Component Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 // import Home from "./Pages/Home";
 // import ProductList from "./Pages/ProductList";
-import Product from "./Pages/Product";
+// import Product from "./Pages/Product";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 
 const App = () => {
   return (
     <>
       {/* <Home /> */}
       {/* <ProductList /> */}
-      <Product/>
+      {/* <Product /> */}
+      <Register />
+      <Login />
     </>
   );
 };
