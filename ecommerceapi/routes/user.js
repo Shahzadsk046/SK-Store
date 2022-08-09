294f2cd9333933a748ba3f382e47cf03e5343700
Shehzad Khan <55661874+Shahzadsk046@users.noreply.github.com>
2022-08-09 19:00:39 +0500

    User Stats added and User Routes Completed

--- ecommerceapi/routes/user.js
+++ ecommerceapi/routes/user.js
@@ -1,5 +1,5 @@
 const {
-  verifyToken,
+  // verifyToken,
   verifyTokenAndAuthorization,
   verifyTokenAndAdmin,
 } = require("./verifyToken");
@@ -60,6 +60,31 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
       ? await User.find().sort({ _id: -1 }).limit(5)
       : await User.find();
     res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET USER STATS
+router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() -1));
+  try {
+    const data = await User.aggregate([
+      {$match: {createdAt: {$gte: lastYear}}},
+  {
+    $project: {
+      month: { $month: "$createdAt"},
+    },
+  },
+    {
+      $group: {
+        _id: "$month",
+        total: { $sum: 1 },
+      },
+    
+  }]);
+    res.status(200).json(data);
   } catch (err) {
     res.status(500).json(err);
   }
