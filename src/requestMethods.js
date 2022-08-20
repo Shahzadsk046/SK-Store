81cf2a656e6dd66f716a6e19f45f818ee7420b9f
Shehzad Khan <55661874+Shahzadsk046@users.noreply.github.com>
2022-08-20 17:57:21 +0500

    Dynamic Token added to the client side

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZjI2MGNiY2UxZDg5MWFmNzY0ZDVlMCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2MDQ4MzEzNSwiZXhwIjoxNjYwNzQyMzM1fQ.DKxx_0APahyPA8YyCzl9CRP3tv5RCGuqpuzLEll-Mk8";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
