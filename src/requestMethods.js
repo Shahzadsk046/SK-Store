dc25b1829ab8566a23197ffc6175fb9c87607420
Shehzad Khan <55661874+Shahzadsk046@users.noreply.github.com>
2022-08-27 15:38:38 +0500

    Access token updated

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-// const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZjI2MGNiY2UxZDg5MWFmNzY0ZDVlMCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2MTUyMjUwMSwiZXhwIjoxNjYxNzgxNzAxfQ.BFOw_31giww8bMYPyBPxHme0_NlYjXPlwcs9edMS6N8";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
+// const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZjI2MGNiY2UxZDg5MWFmNzY0ZDVlMCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2MTUyMjUwMSwiZXhwIjoxNjYxNzgxNzAxfQ.BFOw_31giww8bMYPyBPxHme0_NlYjXPlwcs9edMS6N8";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
